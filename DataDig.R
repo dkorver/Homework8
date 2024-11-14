7829ae4de6b9b5c30d35778c88cf9453a3ae0e0b
dkorver <danekorver@outlook.com>
2024-11-13 21:35:10 -0500

    writing quarto doc

--- DataDig.R
+++ DataDig.R
@@ -1,6 +1,7 @@
 
 library(tidyverse)
 library(janitor)
+library(tidymodels)
 bike_data <- read_csv("https://www4.stat.ncsu.edu/online/datasets/SeoulBikeData.csv",locale = locale(encoding = "ISO-8859-1"))
 bike_data <- bike_data %>% janitor::clean_names()
 
@@ -19,5 +20,20 @@ summary(bike_data)
 bike_data$date_new<-as.Date(bike_data$date,format="%m/%d/%y")
 bike_data <- bike_data %>% mutate(across(where(is.character), as.factor))
 summary(bike_data$rented_bike_count)
+
+set.seed(123)
+cell_split <- initial_split(cells %>% select(-case),strata = class)
+cell_train <- training(cell_split)
+cell_test  <- testing(cell_split)
+nrow(cell_train)
+nrow(cell_train)/nrow(cells)
+cell_train %>% 
+  count(class) %>% 
+  mutate(prop = n/sum(n))
+
+cell_test %>% 
+  count(class) %>% 
+  mutate(prop = n/sum(n))
+
 
 
