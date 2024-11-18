4716bb005d2dd306a9ab51a0664e262f06d05aa3
dkorver <danekorver@outlook.com>
2024-11-18 12:39:54 -0500

    playing around with the data

--- DataDig.R
+++ DataDig.R
@@ -2,11 +2,22 @@
 library(tidyverse)
 library(janitor)
 library(tidymodels)
+library(GGally)
 bike_data <- read_csv("https://www4.stat.ncsu.edu/online/datasets/SeoulBikeData.csv",locale = locale(encoding = "ISO-8859-1"))
 bike_data <- bike_data %>% janitor::clean_names()
+#format the date variable to be an actual date variable
+bike_data$date_new<-as.Date(bike_data$date,format="%m/%d/%y")
+#convert character variables to factor
+bike_data <- bike_data %>% mutate(across(where(is.character), as.factor))
+bike_data_numVars <- 
+  bike_data %>%
+  filter(functioning_day=="Yes") %>%
+  select(-date,-seasons,-holiday,-functioning_day,-date_new) 
 
 class(bike_data)
 typeof(bike_data)
+attributes(bike_data)
+names(bike_data)
 
 sum_na <- function(column){
   sum(is.na(column))
@@ -14,18 +25,43 @@ sum_na <- function(column){
 
 na_counts <- bike_data |>
   summarize(across(everything(), sum_na))
-na_counts
+summary(na_counts)
 
 summary(bike_data)
-bike_data$date_new<-as.Date(bike_data$date,format="%m/%d/%y")
-bike_data <- bike_data %>% mutate(across(where(is.character), as.factor))
 summary(bike_data$rented_bike_count)
+table(bike_data$seasons)
+table(bike_data$holiday)
+table(bike_data$functioning_day)
+cor(bike_data_numVars)
+
+bike_data_for_model <- bike_data %>%
+  group_by(date_new,seasons,holiday) %>%
+  filter(functioning_day=="Yes") %>%
+  summarize(bike_count = sum(rented_bike_count),
+            temp = mean(temperature_c),
+            humidity = mean(humidity_percent),
+            wind_speed = mean(wind_speed_m_s),
+            vis = mean(visibility_10m),
+            dew_point_temp = mean(dew_point_temperature_c),
+            solar_radiation = mean(solar_radiation_mj_m2),
+            rainfall = sum(rainfall_mm),
+            snowfall = sum(snowfall_cm)) %>%
+  ungroup()
+
+bike_data_for_model_numVars <- bike_data_for_model %>%
+  select(-seasons,-holiday,-date_new) 
+
+
+
+
 
 set.seed(123)
-cell_split <- initial_split(cells %>% select(-case),strata = class)
-cell_train <- training(cell_split)
-cell_test  <- testing(cell_split)
-nrow(cell_train)
+bike_split <- initial_split(bike_data, prop = 0.75, strata = seasons)
+bike_train <- training(bike_split)
+bike_test <- testing(bike_split)
+bike_10_fold <- vfold_cv(bike_train, 10)
+
+
 nrow(cell_train)/nrow(cells)
 cell_train %>% 
   count(class) %>% 
@@ -34,6 +70,5 @@ cell_train %>%
 cell_test %>% 
   count(class) %>% 
   mutate(prop = n/sum(n))
-
 
 
